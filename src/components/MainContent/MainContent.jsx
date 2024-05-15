1e69dea9a95b35f7247244bcb506b8c20bf46b22
Manuel-25 <manuelotamendi97@gmail.com>
2024-05-15 20:20:45 -0300

    MainContent implemented

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import QuoteButton from '../QuoteButton/QuoteButton';
+import './MainContent.css'
 
 function MainContent() {
     return (
         <div className="main-content">
-            
+            <section className="section-reparation">
+                <div className='reparation-top'>
+                    <h2>¡Reparamos tu equipo!</h2>
+                    <p>¿Se rompió tu equipo?</p>
+                </div>
+                <div className="reparation-bottom">
+                    <input type="text" placeholder="Describe el problema..." />
+                    <QuoteButton text="Cotizar Ahora" />
+                </div>
+            </section>
         </div>
     )
 }
