fa2fef8dfb954b99e93ca90fba7fd37e0a78401a
Manuel-25 <manuelotamendi97@gmail.com>
2024-05-27 19:04:59 -0300

    Service componentents updated css

--- src/components/InformationForm/InformationForm.jsx
+++ src/components/InformationForm/InformationForm.jsx
@@ -1,79 +1,88 @@
-import React, { useState } from 'react'
-import './InformationForm.css' // Asegúrate de tener el archivo CSS para los estilos
+import React, { useState } from 'react';
+import './InformationForm.css'; // Asegúrate de tener el archivo CSS para los estilos
 
 const InformationForm = ({ nextStep, prevStep, updateFormData }) => {
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [email, setEmail] = useState('')
-  const [phone, setPhone] = useState('')
-  const [province, setProvince] = useState('')
-  const [discountCode, setDiscountCode] = useState('')
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [province, setProvince] = useState('');
+  const [discountCode, setDiscountCode] = useState('');
+  const [additionalDetails, setAdditionalDetails] = useState('');
 
-  // Lista de provincias de Argentina
   const provinces = [
     'Buenos Aires', 'Catamarca', 'Chaco', 'Chubut', 'Córdoba', 'Corrientes', 'Entre Ríos',
     'Formosa', 'Jujuy', 'La Pampa', 'La Rioja', 'Mendoza', 'Misiones', 'Neuquén', 'Río Negro',
     'Salta', 'San Juan', 'San Luis', 'Santa Cruz', 'Santa Fe', 'Santiago del Estero', 'Tierra del Fuego', 'Tucumán'
-  ]
+  ];
 
   const handleProvinceSelect = (event) => {
-    setProvince(event.target.value)
-  }
+    setProvince(event.target.value);
+  };
 
   const handleSubmit = () => {
-    // Aquí puedes enviar los datos del formulario
     const formData = {
       firstName,
       lastName,
       email,
       phone,
       province,
-      discountCode
-    }
-    // Actualizar los datos del formulario en el componente padre
-    updateFormData(formData)
-    // Avanzar a la siguiente etapa
-    nextStep()
-  }
+      discountCode,
+      additionalDetails
+    };
+    updateFormData(formData);
+    nextStep();
+  };
 
   return (
     <div className="selection-container">
       <h2>Te pedimos tus datos, para compartirte la cotización</h2>
-      <div className="form-group">
-        <label>Nombre:</label>
-        <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+      <div className="form-group-wrapper">
+        <div className="form-group floating-label">
+          <input type="text" placeholder='Nombre' value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
+          <label>Nombre</label>
+        </div>
+        <div className="form-group floating-label">
+          <input type="text" placeholder='Apellido' value={lastName} onChange={(e) => setLastName(e.target.value)} required />
+          <label>Apellido</label>
+        </div>
+        <div className="form-group floating-label">
+          <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <label>Email</label>
+        </div>
+        <div className="form-group floating-label">
+          <input type="tel" placeholder='Teléfono' value={phone} onChange={(e) => setPhone(e.target.value)} required />
+          <label>Teléfono</label>
+        </div>
+        <div className="form-group floating-label">
+          <select id='select-provincia' value={province} onChange={handleProvinceSelect} required>
+            <option value="">Provincia</option>
+            {provinces.map((province, index) => (
+              <option key={index} value={province}>{province}</option>
+            ))}
+          </select>
+          <label>Provincia</label>
+        </div>
+        <div className="form-group floating-label">
+          <textarea
+            value={additionalDetails}
+            onChange={(e) => setAdditionalDetails(e.target.value)}
+            required
+            placeholder="Especifique detalles adicionales como marca o modelo"
+          />
+          <label>Detalles adicionales</label>
+        </div>
       </div>
-      <div className="form-group">
-        <label>Apellido:</label>
-        <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label>Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label>Teléfono:</label>
-        <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label>Provincia:</label>
-        <select value={province} onChange={handleProvinceSelect}>
-          <option value="">Seleccionar</option>
-          {provinces.map((province, index) => (
-            <option key={index} value={province}>{province}</option>
-          ))}
-        </select>
-      </div>
-      <div className="form-group">
-        <label>Si tenés un código de descuento, ingrésalo aquí:</label>
-        <input type="text" value={discountCode} onChange={(e) => setDiscountCode(e.target.value)} />
+      <div className="form-group floating-label" id='form-discount'>
+        <span id='form-discount-span'>Si tenés un código de descuento, ingrésalo aquí:</span>
+        <input type="text" placeholder='Codigo de descuento' value={discountCode} onChange={(e) => setDiscountCode(e.target.value)} />
       </div>
       <div className='nav-buttons'>
         <button className="back-button" onClick={prevStep}>Volver</button>
         <button onClick={handleSubmit}>Finalizar</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default InformationForm
+export default InformationForm;
--- src/components/ModelSelection/ModelSelection.jsx
+++ src/components/ModelSelection/ModelSelection.jsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react'
-import './ModelSelection.css'
+import React, { useState } from 'react';
+import './ModelSelection.css';
 
 const ModelSelection = ({ brand, selectedCategory, nextStep, prevStep, updateFormData }) => {
-  const [model, setModel] = useState('')
+  const [model, setModel] = useState('');
 
   const handleModelSelect = (event) => {
-    setModel(event.target.value)
-  }
+    setModel(event.target.value);
+  };
 
   const handleSubmit = () => {
-    updateFormData('model', model)
-    nextStep()
-  }
+    updateFormData('model', model);
+    nextStep();
+  };
 
   return (
     <div className="selection-container">
       <h2>¿Qué modelo es tu {brand === 'Otros' ? selectedCategory.name : brand || selectedCategory.name}?</h2>
+      <p className="instructions">
+        Para encontrar el modelo de tu {selectedCategory.name.toLowerCase()}, revisa la etiqueta del fabricante que
+        generalmente se encuentra en la parte posterior o inferior del dispositivo. Ejemplos comunes incluyen
+        "SM-G950F" para smartphones o "UE40MU6120" para televisores.
+      </p>
       <input 
         type="text" 
         value={model} 
@@ -25,10 +30,10 @@ const ModelSelection = ({ brand, selectedCategory, nextStep, prevStep, updateFor
       />
       <div className='nav-buttons'>
         <button className="back-button" onClick={prevStep}>Volver</button>
-        <button onClick={handleSubmit}>Siguiente</button>
+        <button onClick={handleSubmit} className={model ? 'completed' : ''}>Siguiente</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ModelSelection
+export default ModelSelection;
