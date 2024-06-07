03300c7aa161fc419dd80ed098fa659a77f82acd
Manuel-25 <manuelotamendi97@gmail.com>
2024-06-07 16:20:41 -0300

    minor css changes

--- frontend/src/utils/productsData.jsx
+++ frontend/src/utils/productsData.jsx
@@ -1,19 +1,19 @@
 export const products = [
     { id: 1, name: 'Smartphone' },
-    { id: 2, name: 'Consola' },
     { id: 3, name: 'Televisor' },
-    { id: 4, name: 'Horno Eléctrico' },
+    { id: 2, name: 'Consola' },
+    { id: 11, name: 'Notebook' },
+    { id: 15, name: 'Microondas' },
+    { id: 4, name: 'Horno Electrico' },
     { id: 5, name: 'Cafetera' },
-    { id: 6, name: 'Pava Eléctrica' },
+    { id: 6, name: 'Pava Electrica' },
     { id: 7, name: 'Tostadora' },
     { id: 8, name: 'Plancha' },
     { id: 9, name: 'Secadora de Pelo' },
     { id: 10, name: 'Planchita de Pelo' },
-    { id: 11, name: 'Notebook' },
     { id: 12, name: 'Cava de Vino' },
     { id: 13, name: 'Ventilador' },
-    { id: 14, name: 'Estufa' },
-    { id: 15, name: 'Microondas' }
+    { id: 14, name: 'Estufa' }
 ]
 
 export const brandsByCategory = {
