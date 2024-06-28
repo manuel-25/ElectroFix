7eae4a2e162826c3488109d0ec8579f9e9806e6e
Manuel-25 <manuelotamendi97@gmail.com>
2024-06-28 17:36:38 -0300

    added oauth2client

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,14 +1,44 @@
 import nodemailer from 'nodemailer';
 import config from '../utils/config.js';
+import { OAuth2Client } from 'google-auth-library'
+
+// Configure OAuth2 client
+const oAuth2Client = new OAuth2Client(
+  config.CLIENT_ID,
+  config.CLIENT_SECRET,
+  'https://developers.google.com/oauthplayground'
+)
+
+oAuth2Client.setCredentials({ refresh_token: config.REFRESH_TOKEN })
+
+// Get the access token
+const getAccessToken = async () => {
+  const res = await oAuth2Client.getAccessToken();
+  return res.token;
+}
 
 // Configuración del transporter de Nodemailer
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+  service: 'gmail',
   auth: {
-    user: 'electrosafeservice@gmail.com',
-    pass: config.GMAIL_PASS
+    type: 'OAuth2',
+    user: "electrosafeservice@gmail.com",
+    clientId: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET,
+    refreshToken: config.REFRESH_TOKEN,
+    accessToken: await getAccessToken()
+  },
+  tls: {
+    rejectUnauthorized: false
   }
-});
+})
+
+transporter.on('token', token => {
+  console.log('A new access token was generated');
+  console.log('User: %s', token.user);
+  console.log('Access Token: %s', token.accessToken);
+  console.log('Expires: %s', new Date(token.expires));
+})
 
 // Función para enviar correos electrónicos
 export const sendEmail = async (to, subject, htmlContent) => {
