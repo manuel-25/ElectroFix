c7623251de3e9665658c5dd2db83bbb0d6bbf16e
Manuel-25 <manuelotamendi97@gmail.com>
2024-06-28 18:11:50 -0300

    added credentials

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,29 +1,43 @@
-import { google } from 'googleapis';
 import nodemailer from 'nodemailer';
+import { google } from 'googleapis'; // Importa googleapis en lugar de google-auth-library
 import config from '../utils/config.js';
 
-// Configurar OAuth2 utilizando googleapis
+// Configura el cliente OAuth2
 const oAuth2Client = new google.auth.OAuth2(
   config.CLIENT_ID,
   config.CLIENT_SECRET,
-  'https://developers.google.com/oauthplayground' // Redireccionamiento autorizado
+  'https://developers.google.com/oauthplayground'
 );
+oAuth2Client.setCredentials({ refresh_token: config.REFRESH_TOKEN });
+
+// Función para obtener el ACCESS_TOKEN
+const getAccessToken = async () => {
+  try {
+    const { token } = await oAuth2Client.getAccessToken();
+    return token;
+  } catch (error) {
+    console.error('Error obteniendo ACCESS_TOKEN:', error.message);
+    throw new Error('No se pudo obtener el ACCESS_TOKEN');
+  }
+};
 
 // Configuración del transporter de Nodemailer
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     type: 'OAuth2',
-    user: 'electrosafeservice@gmail.com',
+    user: "electrosafeservice@gmail.com",
     clientId: config.CLIENT_ID,
     clientSecret: config.CLIENT_SECRET,
     refreshToken: config.REFRESH_TOKEN,
-    accessToken: config.ACCESS_TOKEN, // Puedes inicializar esto si ya tienes un ACCESS_TOKEN válido
-    expires: 3600, // Tiempo de expiración en segundos
+    accessToken: await getAccessToken()
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
-// Escuchar eventos de actualización de token (opcional)
+// Escucha eventos de actualización de token (opcional)
 transporter.on('token', token => {
   console.log('Se generó un nuevo ACCESS_TOKEN');
   console.log('Usuario: %s', token.user);
@@ -35,7 +49,7 @@ transporter.on('token', token => {
 export const sendEmail = async (to, subject, htmlContent) => {
   try {
     const mailOptions = {
-      from: 'electrosafeservice@gmail.com',
+      from: "electrosafeservice@gmail.com",
       to: to,
       subject: subject,
       html: htmlContent,
