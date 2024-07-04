43ff2dd674353d706fedca50d6357374279c2046
Manuel-25 <manuelotamendi97@gmail.com>
2024-07-04 16:33:11 -0300

    nodemailer added

--- backend/src/routes/serviceRequests.js
+++ backend/src/routes/serviceRequests.js
@@ -14,15 +14,15 @@ router.post('/', async (req, res) => {
     // Formatear los datos del request body para el correo electrónico
     const { date, category, brand, model, faults, userData } = req.body
     const emailContent = `
-      <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; background-color: #F5F7FA; padding: 20px; border-radius: 10px;">
-        <h2 style="background-color: #70757A; color: white; padding: 10px; text-align: center; border-radius: 10px 10px 0 0;">Nueva Solicitud de Servicio</h2>
+      <div style="font-family: Arial, sans-serif max-width: 600px margin: auto background-color: #F5F7FA padding: 20px border-radius: 10px">
+        <h2 style="background-color: #70757A color: white padding: 10px text-align: center border-radius: 10px 10px 0 0">Nueva Solicitud de Servicio</h2>
         
         <p><b>Fecha:</b> ${new Date(date).toLocaleString()}</p>
         <p><b>Equipo:</b> ${category.name}</p>
         <p><b>Marca:</b> ${brand}</p>
         <p><b>Modelo:</b> ${model}</p>
         <p><b>Fallas reportadas:</b></p>
-        <ul style="background-color: white; padding: 10px; border-radius: 5px;">
+        <ul style="background-color: white padding: 10px border-radius: 5px">
           ${faults.map(fault => `<li>${fault}</li>`).join('')}
         </ul>
         <p><b>Detalles adicionales:</b> ${userData.additionalDetails || 'N/A'}</p>
@@ -35,9 +35,9 @@ router.post('/', async (req, res) => {
         <p><b>Municipio:</b> ${userData.municipio}</p>
         <p><b>Código de descuento:</b> ${userData.discountCode || 'N/A'}</p>
         
-        <p style="text-align: center;">
+        <p style="text-align: center">
           <a href="https://wa.me/${userData.phone}?text=Hola, ${userData.firstName}! te escribo desde Electrosafe ví tu cotizacioón en nuestra web y quería comentarte las opciones y promociones que tenemos para reparación de tu ${category.name}." 
-             style="background-color: #25D366; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block; margin-top: 20px;">
+             style="background-color: #25D366 color: white padding: 10px 20px text-decoration: none border-radius: 5px display: inline-block margin-top: 20px">
             Contactar por WhatsApp
           </a>
         </p>
@@ -45,9 +45,9 @@ router.post('/', async (req, res) => {
     `
 
     // Verifica que el destinatario esté definido
-    const recipientEmail = "electrosafeservice@gmail.com" //config.GMAIL_USER;  // O el correo electrónico que desees usar
+    const recipientEmail = "electrosafeservice@gmail.com" //config.GMAIL_USER  // O el correo electrónico que desees usar
     if (!recipientEmail) {
-      throw new Error('No recipient email defined');
+      throw new Error('No recipient email defined')
     }
 
     // Enviar correo electrónicocd
@@ -60,10 +60,10 @@ router.post('/', async (req, res) => {
 
     res.status(201).send(serviceRequest)
   } catch (error) {
-    console.error('Error creating service request:', error);
+    console.error('Error creating service request:', error)
 
     // Envía la información completa del error en la respuesta
-    res.status(400).send({ error: error.message, stack: error.stack });
+    res.status(400).send({ error: error.message, stack: error.stack })
   }
 })
 
--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,67 +1,33 @@
-import nodemailer from 'nodemailer';
-import { google } from 'googleapis'; // Importa googleapis en lugar de google-auth-library
-import config from '../utils/config.js';
-
-// Configura el cliente OAuth2
-const oAuth2Client = new google.auth.OAuth2(
-  config.CLIENT_ID,
-  config.CLIENT_SECRET,
-  'https://developers.google.com/oauthplayground'
-);
-oAuth2Client.setCredentials({ refresh_token: config.REFRESH_TOKEN });
-
-// Función para obtener el ACCESS_TOKEN
-const getAccessToken = async () => {
-  try {
-    const { token } = await oAuth2Client.getAccessToken();
-    return token;
-  } catch (error) {
-    console.error('Error obteniendo ACCESS_TOKEN:', error.message);
-    throw new Error('No se pudo obtener el ACCESS_TOKEN');
-  }
-};
+import nodemailer from 'nodemailer'
+import config from '../utils/config.js'
 
 // Configuración del transporter de Nodemailer
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587, // Puede ser 465 para SSL/TLS o 587 para STARTTLS
+  secure: false, // true para port 465, false para otros puertos
   auth: {
-    type: 'OAuth2',
-    user: "electrosafeservice@gmail.com",
-    clientId: config.CLIENT_ID,
-    clientSecret: config.CLIENT_SECRET,
-    refreshToken: config.REFRESH_TOKEN,
-    accessToken: await getAccessToken()
-  },
-  tls: {
-    rejectUnauthorized: false
+    user: config.GMAIL_USER,
+    pass: config.GMAIL_PASS
   }
-});
-
-// Escucha eventos de actualización de token (opcional)
-transporter.on('token', token => {
-  console.log('Se generó un nuevo ACCESS_TOKEN');
-  console.log('Usuario: %s', token.user);
-  console.log('ACCESS_TOKEN: %s', token.accessToken);
-  console.log('Expira: %s', new Date(token.expires));
-});
+})
 
 // Función para enviar correos electrónicos
 export const sendEmail = async (to, subject, htmlContent) => {
   try {
     const mailOptions = {
-      from: "electrosafeservice@gmail.com",
+      from: config.GMAIL_USER,
       to: to,
       subject: subject,
       html: htmlContent,
-    };
+    }
 
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Correo enviado: ', info.response);
+    const info = await transporter.sendMail(mailOptions)
+    console.log('Correo enviado: ', info.response)
 
-    return info;
+    return info
   } catch (error) {
-    console.error('Error enviando correo:', error.message);
-    console.error('Detalles completos del error:', error);
-    throw new Error('Fallo al enviar el correo');
+    console.error('Error enviando correo:', error)
+    throw new Error('Fallo al enviar el correo')
   }
-};
+}
