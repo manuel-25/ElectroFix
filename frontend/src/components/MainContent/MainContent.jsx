17f8988846a5167ddd883a427f6833255ea59be0
manuel-25 <manuelotamendi97@gmail.com>
2024-12-09 19:16:22 -0300

    added lodash

--- frontend/src/components/MainContent/MainContent.jsx
+++ frontend/src/components/MainContent/MainContent.jsx
@@ -7,6 +7,7 @@ import { faCircleCheck, faStore, faTruck, faHome, faHandHoldingDollar } from '@f
 import { Link, useNavigate } from 'react-router-dom'
 import { brandLogos, reviews, detailedBrandsByCategory } from '../../utils/productsData'
 import MiniBanner from '../MiniBanner/MiniBanner'
+import _ from 'lodash'
 
 function MainContent() {
   const [searchTerm, setSearchTerm] = useState('')
