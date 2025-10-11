9cd239a9c07ad391e6dc81dd846d28b08c768b24
Manuel-25 <manuelotamendi97@gmail.com>
2025-10-11 12:01:16 -0300

    Hotfix removed logs authetnicatejwt

--- backend/src/middlewares/authenticateJWT.js
+++ backend/src/middlewares/authenticateJWT.js
@@ -3,20 +3,14 @@ import config from '../utils/config.js'
 
 const authenticateJWT = (req, res, next) => {
   const token = req.cookies?.authToken
-  console.log('[AUTH MIDDLEWARE] Token recibido en cookie]', token)
-
   if (!token) {
-    console.log('[AUTH MIDDLEWARE] No hay token]')
     return res.sendStatus(401)
   }
 
   jwt.verify(token, config.JWT_SECRET, (err, decoded) => {
     if (err) {
-      console.log('[AUTH MIDDLEWARE] Token inválido o expirado]', err.message)
       return res.sendStatus(403)
     }
-    console.log('[AUTH MIDDLEWARE] Token válido decoded]', decoded)
-
     req.user = {
       _id: decoded.id,
       email: decoded.email,
